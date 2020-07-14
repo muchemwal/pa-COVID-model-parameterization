8b4bfcd734ae420a94ad7a31bf733135b3063a0b
Leonardo Milano <milanoleonardo@yahoo.it>
2020-07-14 10:43:48 +0200

    adding COVID files to repo

--- run_all_countries.sh
+++ run_all_countries.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for iso3 in AFG SSD SDN COD SOM
 do
-    python Generate_SADD_exposure_from_tiff.py $iso3
-    python Generate_vulnerability_file.py $iso3
+    # python Generate_SADD_exposure_from_tiff.py $iso3
+    # python Generate_vulnerability_file.py $iso3
     python Generate_COVID_file.py -d $iso3
     # python Generate_graph.py -m https://raw.githubusercontent.com/OCHA-DAP/pa-movement-patterns-matrix/master/output/${iso3,,}/${iso3,,}_mobility_matrix.csv $iso3
 done
