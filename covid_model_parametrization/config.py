4c4b794f0d70809d73208d6a1641397028e0b8b3
Monica Turner <turnermon@gmail.com>
2020-08-17 11:19:16 +0200

    Switch to using new WHO file and HDX API

--- covid_model_parametrization/config.py
+++ covid_model_parametrization/config.py
@@ -127,8 +127,6 @@ class Config:
 
     PSEUDO_MERCATOR_CRS = "EPSG:3857"
 
-    WHO_COVID_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSe-8lf6l_ShJHvd126J-jGti992SUbNLu-kmJfx1IRkvma_r4DHi0bwEW89opArs8ZkSY5G2-Bc1yT/pub?gid=0&single=true&output=csv'
-
     ####################### NPI section
 
     ACAPS_HDX_ADDRESS = 'acaps-covid19-government-measures-dataset'
@@ -140,3 +138,10 @@ class Config:
     NPI_INTERMEDIATE_OUTPUT_FILENAME = '{}_NPIs_input.xlsx'
     NPI_TRIAGED_INTERMEDIATE_OUTPUT_FILENAME = '{}_NPIs_triaged.csv'
     NPI_FINAL_OUTPUT_FILENAME = '{}_NPIs.csv'
+
+
+    #################### MISC
+
+    WHO_HDX_ADDRESS = 'coronavirus-covid-19-cases-and-deaths'
+    WHO_DIR = 'WHO_COVID_cases'
+    WHO_FILENAME = 'WHO_covid_cases.csv'
---
+++ covid_model_parametrization/who.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+import logging
+
+import pandas as pd
+from hdx.location.country import Country
+
+from covid_model_parametrization.hdx_api import query_api
+
+logger = logging.getLogger(__name__)
+
+
+def get_WHO_data(config, country_iso3):
+    # Download the file and move it to a local directory
+    logger.info('Downloading WHO data from HDX')
+    WHO_dir = os.path.join(config.INPUT_DIR, config.WHO_DIR)
+    Path(WHO_dir).mkdir(parents=True, exist_ok=True)
+    download_filename = list(query_api(config.WHO_HDX_ADDRESS, WHO_dir, resource_format='CSV').values())[0]
+    final_filepath = os.path.join(WHO_dir, config.WHO_FILENAME)
+    os.rename(os.path.join(WHO_dir, download_filename), final_filepath)
+    # Get the data for the country based on ISO3
+    logger.info(f'Returning WHO data for {country_iso3}')
+    df_WHO = pd.read_csv(final_filepath)
+    return df_WHO.loc[df_WHO[' Country_code'] == Country.get_iso2_from_iso3(country_iso3)]
